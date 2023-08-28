97bc55b16f742394964bd79777a92f9ddae0adfa
mpb2001 <mohamad1381pooya@gmail.com>
2023-08-28 23:28:02 +0330

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,26 @@
+import { Button, Container, Form } from 'react-bootstrap'
+import React, { useRef } from 'react'
+import {v4 as uuidV4} from 'uuid'
+export default function Login({onIdSubmit}) {
+
+    const idRef = useRef()
+    function handleSubmit(e) {
+        e.preventDefault()
+        onIdSubmit(idRef.current.value)
+    }
+    function createNewId() {
+        onIdSubmit(uuidV4())
+    }
+    return (
+        <Container className="align-items-center d-flex" style={{ height: '100vh' }}>
+            <Form onSubmit={handleSubmit} className="w-100">
+                <Form.Group>
+                    <Form.Label>Enter Your Id</Form.Label>
+                    <Form.Control type="text" ref={idRef} required />
+                </Form.Group>
+                <Button type="submit" className="me-2">Login</Button>
+                <Button onClick={createNewId} variant='secondary' className="m-2">Create A New Id</Button>
+            </Form>
+        </Container>
+    )
+}
---
+++ src/contexts/ContactsContext.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function ContactsContext() {
+  return (
+    <div>ContactsContext</div>
+  )
+}
